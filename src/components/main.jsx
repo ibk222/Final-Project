2ad72574bd7d47b0e555b39e3f77b6783a453937
ibk222 <lolideborah6@gmail.com>
2025-01-17 16:23:11 +0100

    first commit

---
+++ src/components/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+            src="https://img.pikbest.com/wp/202347/copy-space-3d-render-of-black-friday-sale-shopping-bag-and-gift-box-with-on-a-dark-background_9757876.jpg!w700wp"
+            alt="Card"
+            height={500}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container">
+              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <p className="card-text fs-5 d-none d-sm-block ">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
